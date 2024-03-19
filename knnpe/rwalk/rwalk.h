d1e92d1a3a3e435b80584aa639cc40a10c3690e5
Haroldo V. Ribeiro <haroldov@gmail.com>
2024-03-19 15:23:13 -0300

    Delete knnpe/rwalk/rwalk.h

--- knnpe/rwalk/rwalk.h
+++
@@ -1,2 +0,0 @@
-void biased_random_walk(int const* ptr, int const* neighs, int n, int num_walks,
-                        int num_steps,  double p, double q, int seed, int nthread, int* walks);                        
